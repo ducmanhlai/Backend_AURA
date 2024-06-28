f7830112758f5135ca1bb130b48222d81210bc10
Đức Mạnh <72874551+ducmanhlai@users.noreply.github.com>
2024-06-29 01:37:04 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: true });
   app.enableCors({
     origin: '*', // or '*' to allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
